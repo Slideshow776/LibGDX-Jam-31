90d47a819bd7fdf4059a7e21b87c0f9440db1e9f
Sandra Moen <sandramoen01@gmail.com>
2024-12-05 11:10:57 +0100

    changed aspect ratio to mobile portrait, from 16:9 to 9:16

--- lwjgl3/src/main/java/no/sandramoen/libgdx31/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/no/sandramoen/libgdx31/lwjgl3/Lwjgl3Launcher.java
@@ -41,7 +41,7 @@ public class Lwjgl3Launcher {
         if (isFullscreen)
             configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         else
-            setWindowedMode(.6f, configuration);
+            setWindowedMode(.25f, configuration);
         return configuration;
     }
 
@@ -55,7 +55,7 @@ public class Lwjgl3Launcher {
         Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) (dimension.width * percentOfScreenSize);
 
-        float aspectRatio = 16 / 9f;
+        float aspectRatio = 9 / 16f;
         int height = (int) (width / aspectRatio);
 
         System.out.println("[DesktopLauncher] Window dimensions => width: " + width + ", height: " + height);
