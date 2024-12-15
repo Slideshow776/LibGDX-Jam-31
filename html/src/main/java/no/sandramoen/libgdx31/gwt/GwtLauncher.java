687d3d985a7a25ab3191be2673ecf12886d0fe56
Sandra Moen <sandramoen01@gmail.com>
2024-12-15 13:57:00 +0100

    added html preload logo

--- html/src/main/java/no/sandramoen/libgdx31/gwt/GwtLauncher.java
+++ html/src/main/java/no/sandramoen/libgdx31/gwt/GwtLauncher.java
@@ -3,6 +3,11 @@ package no.sandramoen.libgdx31.gwt;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Panel;
+
 import no.sandramoen.libgdx31.MyGdxGame;
 
 /** Launches the GWT application. */
@@ -22,5 +27,20 @@ public class GwtLauncher extends GwtApplication {
         @Override
         public ApplicationListener createApplicationListener () {
             return new MyGdxGame();
+        }
+
+
+        @Override
+        public Preloader.PreloaderCallback getPreloaderCallback() {
+            return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
+        }
+
+
+        @Override
+        protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+            meterPanel.setStyleName("gdx-meter");
+            meterPanel.addStyleName("nostripes");
+            meterStyle.setProperty("backgroundColor", "#ffffff");
+            meterStyle.setProperty("backgroundImage", "none");
         }
 }
